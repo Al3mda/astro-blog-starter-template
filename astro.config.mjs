980ca925f4bc2c10724d90b6f3b14722fccf1d66
Emad Masood <bohneen@gmail.com>
2025-12-17 08:06:54 +0300

    Remove i18n integration to fix build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
+// import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
 import robotsTxt from "astro-robots-txt";
 import cloudflare from "@astrojs/cloudflare";
 
-// i18n locales configuration
-const defaultLocale = "en";
-const locales = {
-  en: "en-US",
-  ar: "ar-SA",
-};
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,14 +14,7 @@ export default defineConfig({
   build: { format: "directory" },
   integrations: [
     mdx(),
-    i18n({
-      locales,
-      defaultLocale,
-      exclude: ["pages/api/**/*", "pages/**/*.md", "pages/**/*.mdx"],
-    }),
     sitemap({
-      i18n: { locales, defaultLocale },
-      filter: filterSitemapByDefaultLocale({ defaultLocale }),
     }),
     robotsTxt({
       sitemaps: [
